eee87552a5793d1e503bd5f61ae257c8a3329b72
Mark Mulder <markmulder@gmail.com>
2018-01-29 12:07:58 +0100

    Make it work with directories

--- ghopen.go
+++ ghopen.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"regexp"
+	"strings"
 )
 
 func main() {
-	// walk up to we find a .git ?
-	// or just run git commands in current directory
-	// cmd := "/usr/local/bin/git config --get remote.origin.url"
 	url, err := exec.Command("git", "config", "remote.origin.url").Output()
 	if nerr, ok := err.(*exec.ExitError); ok {
 		log.Fatalf("Exit error: %s", nerr.Error())
@@ -22,8 +21,28 @@ func main() {
 	if repository == nil {
 		log.Fatalf("No repository found")
 	}
-	// check if current directory contains the .git dir
 	fullUrl := fmt.Sprintf("https://github.com/%s", repository[0][1])
+
+	gitDir, err := exec.Command("git", "rev-parse", "--sq", "--show-toplevel").Output()
+	if nerr, ok := err.(*exec.ExitError); ok {
+		log.Fatalf("Exit error: %s", nerr.Error())
+	}
+	dirString := string(gitDir)
+	dirString = strings.TrimRight(dirString, "\n")
+
+	// check pwd with gitdir
+	pwd := os.Getenv("PWD")
+	if pwd == dirString {
+		// exactly the same, we can open the root page on GH
+		fmt.Println("pwd == gitdir")
+	} else {
+		// index 1 will contain the difference between pwd and git root
+		splitted := strings.SplitAfter(pwd, dirString)
+		fmt.Printf("%#v\n", splitted)
+
+		branch := "master" // TODO: update to get current git branch
+		fullUrl = fmt.Sprintf("%s/tree/%s%s", fullUrl, branch, splitted[1])
+	}
 
 	fmt.Printf("Opening url: %s\n", fullUrl)
 	exec.Command("open", fullUrl).Run()
